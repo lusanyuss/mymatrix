ec8bab2fc391c681c57d8e1c9f569967a254b41b
lusanyuss@163.com <lusanyuss@163.com>
2022-05-11 14:50:39 +0800

    youku开发

--- matrix-gradle-plugin/src/main/kotlin/com/youku/apm/plugin/trace/TraceBuildConstants.kt
+++ matrix-gradle-plugin/src/main/kotlin/com/youku/apm/plugin/trace/TraceBuildConstants.kt
@@ -1,7 +1,7 @@
 package com.youku.apm.plugin.trace
 
 object TraceBuildConstants {
-    const val APM_TRACE_CLASS = "com/tencent/matrix/trace/core/AppMethodBeat"
+    const val APM_TRACE_CLASS = "com/youku/apm/trace/core/AppMethodBeat"
     const val APM_TRACE_ON_WINDOW_FOCUS_METHOD = "onWindowFocusChanged"
     const val APM_TRACE_ATTACH_BASE_CONTEXT = "attachBaseContext"
     const val APM_TRACE_ATTACH_BASE_CONTEXT_ARGS = "(Landroid/content/Context;)V"
@@ -12,10 +12,10 @@ object TraceBuildConstants {
     const val APM_TRACE_V4_ACTIVITY_CLASS = "android/support/v4/app/FragmentActivity"
     const val APM_TRACE_ANDROIDX_ACTIVITY_CLASS = "androidx/appcompat/app/AppCompatActivity"
     const val APM_TRACE_APPLICATION_CLASS = "android/app/Application"
-    const val APM_TRACE_METHOD_BEAT_CLASS = "com/tencent/matrix/trace/core/AppMethodBeat"
+    const val APM_TRACE_METHOD_BEAT_CLASS = "com/youku/apm/trace/core/AppMethodBeat"
     const val APM_TRACE_ON_WINDOW_FOCUS_METHOD_ARGS = "(Z)V"
     val UN_TRACE_CLASS = arrayOf("R.class", "R$", "Manifest", "BuildConfig")
-    const val DEFAULT_BLOCK_TRACE = ("[package]\n" + "-keeppackage android/\n" + "-keeppackage com/tencent/matrix/\n")
+    const val DEFAULT_BLOCK_TRACE = ("[package]\n" + "-keeppackage android/\n" + "-keeppackage com/youku/apm/\n")
     private const val METHOD_ID_MAX = 0xFFFFF
     const val METHOD_ID_DISPATCH = METHOD_ID_MAX - 1
 }
